2fae641b029b33aec121902efaf8ed1033ebc7af
Sasha Savenko <sashasavenko3@gmail.com>
2026-01-07 19:59:46 +0200

    swagger getAllPlants and gitignore dist

--- dist/utils/typeGuards.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createEnumGuard = createEnumGuard;
-function createEnumGuard(list) {
-    return (value) => {
-        return list.includes(value);
-    };
-}
-//it's a factory function which accepts one this -- a tuple of string (list) and return a type-narrowing validator function
