5cabecff571745720e979c60d1167c1038ce47d5
Sasha Savenko <sashasavenko3@gmail.com>
2026-01-12 14:44:03 +0200

    plants{id}

--- src/middlewares/swaggerDocs.ts
+++ src/middlewares/swaggerDocs.ts
@@ -4,7 +4,8 @@ import createHttpError from "http-errors";
 import { Request, Response, NextFunction } from "express";
 import swaggerUI from "swagger-ui-express";
 
-
+//this function can't be async because to has to return a middleware
+// readFileSync is used instead of readFile(which would return promise) to return text
 export const swaggerDocs = () => {
     try {
         const swaggerText = readFileSync(SWAGGER_PATH, "utf-8");
